3cd00faa13decd4ec428fe209eb6e2453698301a
hidayatfahmi <hidayat.fh22@gmail.com>
2024-09-05 22:58:40 +0700

    js asychronous

---
+++ KelasFullstack/Materi/Asynchronous/1test.js
@@ -0,0 +1,18 @@
+// Multi thread 
+// const perkalian = (x,y) => x*y;
+
+// const akar = (x) => perkalian (x,x);
+
+// const pythagoras = (a,b,c) => {
+//    return  akar(a) + akar(b) == akar(c)
+// }
+
+// const akar2 = (x) => {
+//     return Math.sqrt(x);
+// }
+
+// console.log(akar2(81));
+
+// console.log(pythagoras(3,4,5));
+
+
